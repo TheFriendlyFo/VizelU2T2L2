ea65b69142c5649f5f600abbc7befd89bef47727
Seth Vizel <seth.vizel@gmail.com>
2022-10-06 12:32:33 -0400

    Working on student program

--- src/Student.java
+++ src/Student.java
@@ -1,26 +1,27 @@
+import java.util.ArrayList;
+
 public class Student {
 
     /* Instance Variables */
     private final String firstName;
     private final String lastName;
     private int gradYear;
-    private double accumulatedTestScores;
-    private int testScoreCount;
+    private final ArrayList<Double> scores;
+    private int highestInd;
 
     /* Constructor; see Note 1 below */
     public Student(String firstName, String lastName, int gradYear) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.gradYear = gradYear;
-        accumulatedTestScores = 0.0;
-        testScoreCount = 0;
+        scores = new ArrayList<>();
+        highestInd = -1;
     }
 
     /* Getter Methods */
-// Returns firstName
     public String getFirstName() {return firstName;}
-    // Returns lastName
     public String getLastName() {return lastName;}
+    public Double getHighestGrade() {return scores.get(highestInd);}
 
     /* Setter Methods */
 // Sets gradYear to newGradYear
@@ -30,24 +31,31 @@ public class Student {
 // Adds newTestScore to accumulatedTestScores
 // and increments testScoreCount by 1
     public void addTestScore(double newTestScore) {
-        accumulatedTestScores += newTestScore;
-        testScoreCount++;
+        if (highestInd == -1 || newTestScore > scores.get(highestInd)) {
+            highestInd = scores.size();
+        }
+
+        scores.add(newTestScore);
     }
 
     // Returns true if the student’s average test score is greater
 // than or equal to 65; returns false otherwise (see Note 2 below)
     public boolean isPassing() {return averageTestScore() >= 65;}
 
-    // Returns the Student’s average test score as the
-// quotient of accumulatedTestScores and testScoreCount
-    public double averageTestScore() {return accumulatedTestScores / testScoreCount;}
+    public double averageTestScore() {
+        double av = 0;
+
+        for (Double score : scores) {av += score;}
+
+        return av / scores.size();
+    }
 
     // this method prints the students full name, grad year, test average,
 // and whether they are passing (see Note 3 below)
     public void printStudentInfo() {
         System.out.printf("Student: %s %s\n", firstName, lastName);
         System.out.printf("Graduation year: %s\n", gradYear);
-        System.out.printf("Number of tests: %s\n", testScoreCount);
+        System.out.printf("Number of tests: %s\n", scores.size());
         System.out.printf("Test average: %s\n", averageTestScore());
         System.out.printf("Passing: %s\n", isPassing() ? "Yes" : "No");
     }

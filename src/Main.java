b766bed5cf81bf8bbefa699989650e261df439cf
SethV <seth.vizel@gmail.com>
2022-10-06 16:06:51 -0400

    Lab Part 2 Complete

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,74 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Student s;
+        Scanner scan = new Scanner(System.in);
+        boolean run = true;
 
+        // Student initialization:
+        System.out.println("----------------------");
+        System.out.println("Student Initialization");
+        System.out.println("----------------------\n");
 
+        System.out.print("Enter student ID: ");
+        int id = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("Enter student's first name: ");
+        String firstName = scan.nextLine();
+
+        System.out.print("Enter student's last name: ");
+        String lastName = scan.nextLine();
+
+        System.out.print("Enter student's graduation year: ");
+        int gradYear = scan.nextInt();
+        scan.nextLine();
+
+        s = new Student(firstName, lastName, gradYear, id);
+
+        // Main loop:
+        while (run) {
+            // Moves a bunch of lines down to approximate clearing the console:
+            System.out.println("\n\n\n\n\n\n\n\n\n\n");
+
+            System.out.println("[}--------------------------{]");
+            System.out.println("|---------Main Menu----------|");
+            System.out.println("[}--------------------------{]");
+            System.out.println("\n1. Add test score");
+            System.out.println("2. Update graduation date");
+            System.out.println("3. Print test average");
+            System.out.println("4. Print full student information");
+            System.out.println("5. Exit");
+            System.out.print("\nSelection: ");
+
+            switch (scan.nextLine()) {
+                case "1" -> {
+                    System.out.print("Enter new test score: ");
+                    s.addTestScore(scan.nextDouble());
+                    scan.nextLine();
+                }
+                case "2" -> {
+                    System.out.print("Enter new graduation year: ");
+                    s.setGradYear(scan.nextInt());
+                    scan.nextLine();
+                }
+                case "3" -> {
+                    System.out.printf("\nAverage: %s\n\n", s.averageTestScore());
+                    scan.nextLine();
+                }
+                case "4" -> {
+                    System.out.println("\n\n\n\n\n\n\n\n\n\n");
+                    s.printStudentInfo();
+                    scan.nextLine();
+                }
+                case "5" ->
+                    run = false;
+
+                default -> {
+                    System.out.println("Invalid entry.");
+                    scan.nextLine();
+                }
+            }
+        }
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -5,57 +5,64 @@ public class Student {
     /* Instance Variables */
     private final String firstName;
     private final String lastName;
+    private final int id;
     private int gradYear;
-    private final ArrayList<Double> scores;
+    private final ArrayList<Double> grades;
     private int highestInd;
 
-    /* Constructor; see Note 1 below */
-    public Student(String firstName, String lastName, int gradYear) {
+    // Constructor:
+    public Student(String firstName, String lastName, int gradYear, int id) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.id = id;
         this.gradYear = gradYear;
-        scores = new ArrayList<>();
+        grades = new ArrayList<>();
         highestInd = -1;
     }
 
     /* Getter Methods */
     public String getFirstName() {return firstName;}
     public String getLastName() {return lastName;}
-    public Double getHighestGrade() {return scores.get(highestInd);}
-
-    /* Setter Methods */
-// Sets gradYear to newGradYear
-    public void setGradYear(int newGradYear) {gradYear = newGradYear;}
-
-    /* All Other Methods */
-// Adds newTestScore to accumulatedTestScores
-// and increments testScoreCount by 1
-    public void addTestScore(double newTestScore) {
-        if (highestInd == -1 || newTestScore > scores.get(highestInd)) {
-            highestInd = scores.size();
-        }
-
-        scores.add(newTestScore);
-    }
-
-    // Returns true if the student’s average test score is greater
-// than or equal to 65; returns false otherwise (see Note 2 below)
+    public Double getHighestGrade() {return grades.get(highestInd);}
+    public int getNumGrades() {return grades.size();}
+    // Returns true if the student’s average test score is greater than or equal to 65; returns false otherwise
     public boolean isPassing() {return averageTestScore() >= 65;}
 
-    public double averageTestScore() {
-        double av = 0;
-        for (Double score : scores) {av += score;}
-        return av / scores.size();
+    // gradYear setter:
+    public void setGradYear(int newGradYear) {gradYear = newGradYear;}
+
+    // Adds a new score to grades and if necessary update highestInd:
+    public void addTestScore(double newTestScore) {
+        if (highestInd == -1 || newTestScore > grades.get(highestInd)) {
+            highestInd = grades.size();
+        }
+
+        grades.add(newTestScore);
     }
 
-    // this method prints the students full name, grad year, test average,
-// and whether they are passing (see Note 3 below)
+    // Gets the sum of the test scores by looping through them and divides by the number of scores:
+    public double averageTestScore() {
+        double av = 0;
+        for (Double score : grades) {av += score;}
+        return av / grades.size();
+    }
+
+    // Prints all data about the student:
     public void printStudentInfo() {
-        System.out.printf("Student: %s %s\n", firstName, lastName);
+        System.out.printf("Student %s:\n", id);
+        System.out.println("-----------------------");
+        System.out.printf("Student name: %s %s\n", firstName, lastName);
         System.out.printf("Graduation year: %s\n", gradYear);
-        System.out.printf("Number of tests: %s\n", scores.size());
+        System.out.printf("Number of tests: %s\n", getNumGrades());
         System.out.printf("Test average: %s\n", averageTestScore());
-        System.out.printf("Highest score: %s", getHighestGrade());
+        System.out.printf("Highest score: %s\n", getHighestGrade());
         System.out.printf("Passing: %s\n", isPassing() ? "Yes" : "No");
+        System.out.println("-----------------------");
     }
 }
+
+/* NOTE:
+* I did the extra credit task of keeping track of the highest grade, but somewhat weirdly. Instead of storing the sum
+* of all grades, I stored every grade individually in an arraylist, that way in theory you could access them. I know
+* it isn't necessary, but it sounded interesting to try. Anyway, the point is that instead of storing the highest grade
+* itself, I stored the index at which it was found, so it can then be accessed later.*/
